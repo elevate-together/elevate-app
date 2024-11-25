d4693f2d75a72614d8aa86d4df4c9f640da58fcd
Gemma Goddard <gemmagoddard@Gemmas-Laptop.local>
2024-11-25 02:51:45 -0600

    add policy and service

---
+++ app/policy/page.tsx
@@ -0,0 +1,9 @@
+const Policy: React.FC = () => {
+  return (
+    <div>
+      All data is private and will be kept secure, it will not be shared.
+    </div>
+  );
+};
+
+export default Policy;
---
+++ app/service/page.tsx
@@ -0,0 +1,10 @@
+const Service: React.FC = () => {
+  return (
+    <div>
+      I am a christian accountability app in the making. I am learning how to
+      code in next.js
+    </div>
+  );
+};
+
+export default Service;
