cc5c3edebe09285d3867c6680d39cac5d571d7dc
Gemma Goddard <gemma.goddard@tamu.edu>
2024-12-17 10:19:34 -0600

    Fixing Build

--- app/profile/Info.tsx
+++ app/profile/Info.tsx
@@ -1,52 +1,5 @@
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { EllipsisVertical } from "lucide-react";
-import { getOSVersion } from "@utils/getOsVersion";
-import TestNotifyButton from "@components/custom/TestNotifyButton";
 import { auth } from "@auth";
 import { redirect } from "next/navigation";
-import PushNotificationManager from "@components/custom/PushNotificationManager";
-import db from "@lib/db";
-import { toZonedTime } from "date-fns-tz";
-
-async function getUserDevices(userId: string) {
-  try {
-    const user = await db.user.findUnique({
-      where: {
-        id: userId,
-      },
-      include: {
-        devices: true,
-      },
-    });
-
-    if (user) {
-      return {
-        devices: user.devices,
-      };
-    } else {
-      return { devices: [] };
-    }
-  } catch (error) {
-    console.error("Error fetching devices:", error);
-    throw new Error("Failed to fetch devices");
-  }
-}
 
 export const Info = async () => {
   const session = await auth();
@@ -55,23 +8,7 @@ export const Info = async () => {
     redirect("/login");
   }
 
-  const userId = session.user.id;
-
-  const { devices } = await getUserDevices(userId);
-
-  const timeZone = "America/Chicago";
-
-  const dateTimeFormatter = new Intl.DateTimeFormat("en-US", {
-    year: "numeric",
-    month: "short",
-    day: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric",
-    hour12: true,
-    timeZone: timeZone,
-    timeZoneName: "short",
-  });
+  // const userId = session.user.id;
 
   return (
     <div className="container mx-auto p-4 w-full">
