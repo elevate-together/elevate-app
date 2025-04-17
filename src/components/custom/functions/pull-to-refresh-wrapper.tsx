54aed9038ba41954901f0427dd35da677985c957
Gemma Goddard <gemgoddard3@gmail.com>
2025-04-17 17:51:00 -0500

    Trying github bug fix for pull to refresh

--- src/components/custom/functions/pull-to-refresh-wrapper.tsx
+++ src/components/custom/functions/pull-to-refresh-wrapper.tsx
@@ -4,36 +4,65 @@ import { useEffect, useRef } from "react";
 import PullToRefresh from "pulltorefreshjs";
 import { usePathname, useRouter } from "next/navigation";
 
-export function PullToRefreshWrapper({
-  children,
-  className,
-}: {
+interface Props {
   children: React.ReactNode;
   className?: string;
-}) {
+}
+
+export function PullToRefreshWrapper({ children, className }: Props) {
   const router = useRouter();
   const containerRef = useRef<HTMLDivElement>(null);
   const pathname = usePathname();
+  const shouldRefreshRef = useRef(true);
 
   useEffect(() => {
-    const el = containerRef.current;
+    const container = containerRef.current;
 
-    if (el) {
+    const handleTouchStart = (ev: TouchEvent) => {
+      let el = ev.target as HTMLElement | null;
+
+      shouldRefreshRef.current = true;
+
+      while (el && el !== document.body) {
+        if (el.scrollTop > 0) {
+          shouldRefreshRef.current = false;
+          break;
+        }
+
+        // Optional: prevent PTR if interacting with modal/dialog
+        if (el.getAttribute("role") === "dialog") {
+          shouldRefreshRef.current = false;
+          break;
+        }
+
+        el = el.parentElement;
+      }
+    };
+
+    const handleTouchEnd = () => {
+      shouldRefreshRef.current = true;
+    };
+
+    document.body.addEventListener("touchstart", handleTouchStart);
+    document.body.addEventListener("touchend", handleTouchEnd);
+
+    if (container) {
       PullToRefresh.init({
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        mainElement: el as any,
-        shouldPullToRefresh: () => {
-          return el ? el.scrollTop === 0 : false;
-        },
+        mainElement: container as any,
+        shouldPullToRefresh: () =>
+          container.scrollTop === 0 && shouldRefreshRef.current,
         onRefresh: () => {
           router.refresh();
         },
       });
-
-      return () => {
-        PullToRefresh.destroyAll();
-      };
     }
+
+    return () => {
+      PullToRefresh.destroyAll();
+      document.body.removeEventListener("touchstart", handleTouchStart);
+      document.body.removeEventListener("touchend", handleTouchEnd);
+    };
   }, [router, pathname]);
 
   return (
